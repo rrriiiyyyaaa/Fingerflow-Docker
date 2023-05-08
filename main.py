b0e72208bc6a7af8f5887b32700ce100cf5f86c2
Jakub <jakub.przystasz@gmail.com>
2023-05-08 22:12:23 +0200

    Update main.py @mherda64

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import cv2
-import numpy as np
 from fingerflow.extractor import Extractor
 
-extractor = Extractor("CoarseNet.h5", "FineNet.h5", "ClassifyNet_6_classes.h5", "CoreNet.weights")
+extractor = Extractor("./CoarseNet.h5", "./FineNet.h5", "ClassifyNet_6_classes.h5", "./CoreNet.weights")
 
-image = cv2.imread(input("Enter filename"))
+imageName = input("Enter image name (without extension): ") + ".tif"
+
+image = cv2.imread(imageName)
 
 extracted_minutiae = extractor.extract_minutiae(image)
+
+minutiae = extracted_minutiae['minutiae']
+
+for index, minutia in minutiae.iterrows():
+    print("\n ==== ", index, " ==== \n", minutia)
+    imageRGB = cv2.imread(imageName, cv2.IMREAD_COLOR)
+    cv2.circle(imageRGB, (int(minutia['x']), int(minutia['y'])), radius=5, color=(0, 0, 255), thickness=-1)
+    cv2.imshow('image', imageRGB)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
